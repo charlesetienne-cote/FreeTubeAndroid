18c2179026597c737e405aa8309231fbe7303f7c
MarmadileManteater <MarmadileManteater@proton.me>
2022-06-16 08:34:02 -0400

    if dirname doesn't exist

--- _scripts/cordova-build.js
+++ _scripts/cordova-build.js
@@ -36,6 +36,10 @@ const createXMLStringFromObject = function (obj) {
 const archiver = require('archiver');
 
 (async function () {
+  if (await fsExists(__dirname)) {
+    // if dirname doesn't exist, we may be in bizzaro land
+    console.log(__dirname + " doesn't exist");
+  }
   // Remove the dist folder if it already exists
   if (!await fsExists(joinPath(__dirname, '/../build'))) {
     await fsMkdir(joinPath(__dirname, '/../build'))
@@ -84,7 +88,7 @@ const archiver = require('archiver');
   console.log(packageDir);
   const sourcePackage = JSON.parse((await fsReadFile(joinPath(__dirname, '/../', 'package.json'))).toString())
 
-  const destinationPackage = JSON.parse((await fsReadFile(joinPath(__dirname, "/../build/", DIST_FOLDER_NAME, '/package.json'))).toString())
+  const destinationPackage = JSON.parse((await fsReadFile(joinPath(path.join(__dirname, '..'), "/build/", DIST_FOLDER_NAME, '/package.json'))).toString())
   destinationPackage.name = 'io.freetubeapp.' + sourcePackage.name
   destinationPackage.displayName = sourcePackage.productName
   destinationPackage.version = sourcePackage.version
