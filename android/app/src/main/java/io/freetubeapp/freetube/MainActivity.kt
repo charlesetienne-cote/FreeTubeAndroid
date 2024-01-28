6727f6d9b29dcd446f103a513253992d0a6112e7
Emma <MarmadileManteater@proton.me>
2024-01-28 15:26:32 -0500

    Added support for fullscreen
    - is a wip, currently, the video does not take up the whole screen.  I have gotten the navigation and status bar to hide, but they still take up whitespace.

--- android/app/src/main/java/io/freetubeapp/freetube/MainActivity.kt
+++ android/app/src/main/java/io/freetubeapp/freetube/MainActivity.kt
@@ -1,9 +1,14 @@
 package io.freetubeapp.freetube
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
 import io.freetubeapp.freetube.databinding.ActivityMainBinding
@@ -14,7 +19,9 @@ class MainActivity : AppCompatActivity(), OnRequestPermissionsResultCallback {
 
   private lateinit var binding: ActivityMainBinding
   private lateinit var permissionsListeners: MutableList<(Int, Array<String?>, IntArray) -> Unit>
+  private var fullscreenView: View? = null;
   lateinit var webView: BackgroundPlayWebView
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     permissionsListeners = arrayOf<(Int, Array<String?>, IntArray) -> Unit>().toMutableList()
@@ -22,6 +29,7 @@ class MainActivity : AppCompatActivity(), OnRequestPermissionsResultCallback {
     setContentView(binding.root)
 
     webView = binding.webView
+
     webView.settings.javaScriptEnabled = true
     // this is the 🥃 special sauce that makes local api streaming a possibility:
     @Suppress("DEPRECATION")
@@ -31,7 +39,39 @@ class MainActivity : AppCompatActivity(), OnRequestPermissionsResultCallback {
 
     val jsInterface = FreeTubeJavaScriptInterface(this)
     webView.addJavascriptInterface(jsInterface, "Android")
+    webView.webChromeClient = object: WebChromeClient() {
+      override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+        fullscreenView = view!!
+        view.layoutParams = FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+        this@MainActivity.binding.root.addView(view)
+        webView.visibility = View.GONE
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+          window.decorView.windowInsetsController!!.hide(
+            android.view.WindowInsets.Type.statusBars()
+              or android.view.WindowInsets.Type.navigationBars()
+          )
+        } else {
+          window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+          actionBar?.hide()
+        }
+      }
+
+      override fun onHideCustomView() {
+        webView.visibility = View.VISIBLE
+        this@MainActivity.binding.root.removeView(fullscreenView)
+        fullscreenView = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+          window.decorView.windowInsetsController!!.show(
+            android.view.WindowInsets.Type.statusBars()
+              or android.view.WindowInsets.Type.navigationBars()
+          )
+        } else {
+          window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+          actionBar?.show()
+        }
+      }
+    }
     webView.loadUrl("file:///android_asset/index.html")
     Handler(Looper.getMainLooper()).postDelayed({
       webView.loadUrl(
