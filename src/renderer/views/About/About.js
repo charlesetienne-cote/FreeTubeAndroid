b0a8703b96fa91b82651f81075204a24f52e6a5f
Emma <MarmadileManteater@proton.me>
2024-04-08 12:55:33 -0400

    Fix a few leftover references to "FreeTubeCordova"
    - in the about section
    - in the app metadata

--- src/renderer/views/About/About.js
+++ src/renderer/views/About/About.js
@@ -14,7 +14,7 @@ export default defineComponent({
         {
           icon: ['fab', 'github'],
           title: this.$t('About.Source code'),
-          content: `<a href="https://github.com/MarmadileManteater/FreeTubeCordova">GitHub: FreeTubeCordova</a><br>${this.$t('About.Licensed under the AGPLv3')} <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">${this.$t('About.View License')}</a>.<br/> This is a fork of the official <a href="https://github.com/FreeTubeApp/FreeTube">FreeTube</a> repo with modifications to work better in a browser and on phones.`
+          content: `<a href="https://github.com/MarmadileManteater/FreeTubeCordova">GitHub: FreeTubeAndroid</a><br>${this.$t('About.Licensed under the AGPLv3')} <a href="https://www.gnu.org/licenses/agpl-3.0.en.html">${this.$t('About.View License')}</a>.<br/> This is a fork of the official <a href="https://github.com/FreeTubeApp/FreeTube">FreeTube</a> repo with modifications to work better in a browser and on phones.`
         },
         {
           icon: ['fas', 'file-download'],
